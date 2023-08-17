3f3f669528955237d17f8043cd8a645e0bc18196
Martin Miglio <marmig0404@gmail.com>
2023-08-17 15:11:47 -0400

    force dynamic page for index
    add caching and revalidation for db calls
    
    server only on api (db) calls

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { getAllPosts } from "@/api/post";
 import PostsGrid from "@/components/PostsGrid";
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   const posts = await getAllPosts();
   return (
