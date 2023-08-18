f790d7b0d5cc73a830f5af6eafcddc6d87d4ccf5
Martin Miglio <marmig0404@gmail.com>
2023-08-18 01:02:41 -0400

    add api route for fetching posts by slug

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,6 +1,16 @@
-import { createPost } from "@/api/post";
+import { createPost, getPostBySlug } from "@/api/post";
 import { Post } from "@/types/Post/Post";
 import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const slug = searchParams.get("slug");
+  if (!slug) {
+    return NextResponse.error();
+  }
+  const post = await getPostBySlug(slug);
+  return NextResponse.json(post);
+}
 
 export async function POST(req: NextRequest) {
   try {
