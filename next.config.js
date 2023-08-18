05fb98eb27f2d1b01e71bc07d02affc5bc3a4039
Martin Miglio <marmig0404@gmail.com>
2023-08-18 01:10:17 -0400

    allow images from www.toadtopia.rocks

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["via.placeholder.com", "***REMOVED***.s3.us-east-1.amazonaws.com"],
+    domains: [
+      "via.placeholder.com",
+      "***REMOVED***.s3.us-east-1.amazonaws.com",
+      "www.***REMOVED***",
+    ],
   },
 };
 
