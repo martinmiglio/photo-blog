ab13fb7ca132d946afcadcf9d7c242b5c69cc559
Martin Miglio <marmig0404@gmail.com>
2023-08-18 01:24:40 -0400

    style cleanup

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <div className="flex w-full justify-between py-2">
-      <h1 className="mx-auto text-2xl font-bold text-theme-500">
+      <h1 className="mx-auto text-2xl font-bold text-theme-600">
         <Link href="/">***REMOVED***</Link>
       </h1>
       {/* <div className="flex gap-4">
