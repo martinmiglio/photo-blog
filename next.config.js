22b089007f1c0a17522757703df915671b357dc3
Martin Miglio <marmig0404@gmail.com>
2023-08-22 16:50:46 -0400

    update nextjs webpack config

--- next.config.js
+++ next.config.js
@@ -19,6 +19,17 @@ const nextConfig = {
       { protocol: "https", hostname: "**.googleusercontent.com" },
     ],
   },
+  webpack: (config, { webpack, isServer, nextRuntime }) => {
+    // only necessary for warning caused by aws-sdk and turbopack
+    if (isServer && nextRuntime === "nodejs") {
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /^(aws-crt|@aws-sdk\/signature-v4-crt)$/,
+        }),
+      );
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
