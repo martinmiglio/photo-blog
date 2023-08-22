7cc26e5d8ed24e6e7293fd2ef1c593884fcd1e5b
Martin Miglio <marmig0404@gmail.com>
2023-08-21 21:59:52 -0400

    verify user on api route

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,4 +1,5 @@
 import { createPost, getPostBySlug, Post } from "@/db/post";
+import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -12,6 +13,10 @@ export async function GET(req: NextRequest) {
 }
 
 export async function POST(req: NextRequest) {
+  const token = await getToken({ req });
+  if (!token?.poster) {
+    return NextResponse.error();
+  }
   try {
     const post: Post = await req.json();
     console.log(post);
