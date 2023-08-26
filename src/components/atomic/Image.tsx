8200c91bfec95efcdb08cd1aeb9fb3312005c9a0
Martin Miglio <marmig0404@gmail.com>
2023-08-26 13:11:02 -0400

    update image placeholder to match tailwind colors

--- src/components/atomic/Image.tsx
+++ src/components/atomic/Image.tsx
@@ -1,15 +1,19 @@
 import NextImage, { ImageProps } from "next/image";
+import config from "@/../tailwind.config.js";
+
+const { theme } = config.theme.colors;
+
 
 const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
     <linearGradient id="g">
-      <stop stop-color="#F8A5D0" offset="20%" />
-      <stop stop-color="#F8C4DC" offset="50%" />
-      <stop stop-color="#F8A5D0" offset="70%" />
+      <stop stop-color="${theme[300]}" offset="20%" />
+      <stop stop-color="${theme[200]}" offset="50%" />
+      <stop stop-color="${theme[300]}" offset="70%" />
     </linearGradient>
   </defs>
-  <rect width="${w}" height="${h}" fill="#F8A5D0" />
+  <rect width="${w}" height="${h}" fill="${theme[300]}" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
 </svg>`;
